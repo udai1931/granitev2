e16ae5ab0402fd220616641f9e89fd6fd68a987f
Udai Gupta <udaigupta19311@gmail.com>
2022-10-06 15:21:01 +0530

    Implemented create action

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,8 @@
 class ApplicationRecord < ActiveRecord::Base
   include ActionView::Helpers::TranslationHelper
   primary_abstract_class
+
+  def errors_to_sentence
+    errors.full_messages.to_sentence
+  end
 end
